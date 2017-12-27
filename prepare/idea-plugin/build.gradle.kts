f7a7f29a97381eee4c556b67adda77e5f3706d04
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-12-27 18:10:03 +0300

    Build against coroutines bundled in IDEA instead of downloaded

--- prepare/idea-plugin/build.gradle.kts
+++ prepare/idea-plugin/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     sideJars(projectDist(":kotlin-reflect"))
     sideJars(commonDep("io.javaslang", "javaslang"))
     sideJars(commonDep("javax.inject"))
-    sideJars(preloadedDeps("markdown", "kotlinx-coroutines-core", "kotlinx-coroutines-jdk8"))
+    sideJars(preloadedDeps("markdown"))
 }
 
 val jar = runtimeJar(task<ShadowJar>("shadowJar")) {
